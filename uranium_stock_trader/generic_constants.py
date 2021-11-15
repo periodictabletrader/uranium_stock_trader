01ea11e3d087b75131fa323b693091918c56ef36
periodictabletrader <periodictabletrader@gmail.com>
2021-11-15 22:38:03 +0000

    Update generic_constants.py

--- uranium_stock_trader/generic_constants.py
+++ uranium_stock_trader/generic_constants.py
@@ -1,2 +1,4 @@
 
 DB_NAME = '<Update this value and rename this file in place to constants.py>'
+DEFAULT_DOWNLOAD_PATH = '<Update this to the location where you want Selenium to download files>'
+CHROME_DRIVER_PATH = '<Update this to the file location of the chromedriver>'
