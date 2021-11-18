59ae49e14db1550abc5a8848ad1b085ff2fec52f
periodictabletrader <periodictabletrader@gmail.com>
2021-11-18 00:27:42 +0000

    Function to analyse percentage of ETF additions vs avg market traded volume

--- uranium_stock_trader/etf/holdings.py
+++ uranium_stock_trader/etf/holdings.py
@@ -1,3 +1,5 @@
+import datetime
+
 import pandas as pd
 import yfinance as yf
 import sqlite3
@@ -42,3 +44,31 @@ def get_ticker_holding(tickers, start_date=None, end_date=None, etfs=None):
     query = construct_query(tickers, start_date, end_date, etfs)
     df = pd.read_sql_query(query, connection(), parse_dates={'hdate': '%Y-%m-%d'})
     return df
+
+
+def _get_start_and_end_date(start_date, end_date, all_dates):
+    start_date = start_date or pd.Timestamp(min(all_dates)).to_pydatetime()
+    end_date = end_date or pd.Timestamp(max(all_dates)).to_pydatetime()
+    end_date = end_date + datetime.timedelta(days=1)
+    return start_date, end_date
+
+
+def shares_traded_in_etf_vs_mkt(ticker, start_date=None, end_date=None, etfs=None):
+    etfs = wrap_list(etfs)
+    ticker_holding = get_ticker_holding(ticker, start_date, end_date, etfs)
+    ticker_holding = ticker_holding.sort_values('hdate')
+    ticker_holding['shares_delta'] = ticker_holding['shares'].diff()
+    ticker_holding['shares_delta_abs'] = ticker_holding['shares_delta'].abs()
+    if start_date is None or end_date is None:
+        all_dates = ticker_holding['hdate'].unique()
+        start_date, end_date = _get_start_and_end_date(start_date, end_date, all_dates)
+    yf_ticker = yf.Ticker(ticker)
+    yf_start_date = start_date - datetime.timedelta(days=130)
+    ticker_hist = yf_ticker.history(start=yf_start_date, end=end_date)
+    ticker_hist['3MAvgVol'] = ticker_hist['Volume'].rolling(window=90).mean()
+    ticker_overall = pd.merge(ticker_hist, ticker_holding, left_index=True, right_on='hdate')
+    ticker_overall['pct_of_3M_vol'] = ticker_overall['shares_delta'] / ticker_overall['3MAvgVol']
+    ticker_overall['pct_of_3M_vol_abs'] = ticker_overall['shares_delta_abs'] / ticker_overall['3MAvgVol']
+    ticker_overall = ticker_overall[['hdate', 'fund', 'ticker', 'mv', 'shares', 'shares_delta', 'shares_delta_abs',
+                                     '3MAvgVol', 'pct_of_3M_vol', 'pct_of_3M_vol_abs', 'pct_of_nav']]
+    return ticker_overall
