29a4d7c932f9ab8a190791901b5b2e38a53e1250
periodictabletrader <periodictabletrader@gmail.com>
2021-11-18 22:11:09 +0000

    Added Price, Shares Held change columns.

--- uranium_stock_trader/etf/holdings.py
+++ uranium_stock_trader/etf/holdings.py
@@ -43,7 +43,7 @@ def get_ticker_holding(tickers, start_date=None, end_date=None, etfs=None):
     etfs = wrap_list(etfs)
     query = construct_query(tickers, start_date, end_date, etfs)
     df = pd.read_sql_query(query, connection(), parse_dates={'hdate': '%Y-%m-%d'})
-    df = df.rename(columns={'shares': 'shares_held'})
+    df = df.rename(columns={'shares': 'Shares_held'})
     return df
 
 
@@ -58,19 +58,24 @@ def shares_traded_in_etf_vs_mkt(ticker, start_date=None, end_date=None, etfs=Non
     etfs = wrap_list(etfs)
     ticker_holding = get_ticker_holding(ticker, start_date, end_date, etfs)
     ticker_holding = ticker_holding.sort_values('hdate')
-    ticker_holding['shares_held_delta'] = ticker_holding['shares_held'].diff()
-    ticker_holding['shares_held_delta_abs'] = ticker_holding['shares_held_delta'].abs()
+    ticker_holding['Shares_held_change'] = ticker_holding['Shares_held'].diff()
+    ticker_holding['Shares_held_change_abs'] = ticker_holding['Shares_held_change'].abs()
+    ticker_holding['Shares_held_change_pct'] = ticker_holding['Shares_held_change'] / ticker_holding['Shares_held']
     if start_date is None or end_date is None:
         all_dates = ticker_holding['hdate'].unique()
         start_date, end_date = _get_start_and_end_date(start_date, end_date, all_dates)
     yf_ticker = yf.Ticker(ticker)
     yf_start_date = start_date - datetime.timedelta(days=130)
     ticker_hist = yf_ticker.history(start=yf_start_date, end=end_date)
+    ticker_hist['Price_change'] = ticker_hist['Close'] - ticker_hist['Open']
+    ticker_hist['Price_change_pct'] = ticker_hist['Price_change'] / ticker_hist['Open']
+    ticker_hist = ticker_hist.rename(columns={'Close': 'Price'})
     ticker_hist['3MAvgVol'] = ticker_hist['Volume'].rolling(window=90).mean()
     ticker_overall = pd.merge(ticker_hist, ticker_holding, left_index=True, right_on='hdate')
-    ticker_overall['pct_of_3M_vol'] = ticker_overall['shares_held_delta'] / ticker_overall['3MAvgVol']
-    ticker_overall['pct_of_3M_vol_abs'] = ticker_overall['shares_held_delta_abs'] / ticker_overall['3MAvgVol']
-    ticker_overall = ticker_overall[['hdate', 'fund', 'ticker', 'mv', 'shares_held', 'shares_held_delta',
-                                     'shares_held_delta_abs', '3MAvgVol', 'pct_of_3M_vol', 'pct_of_3M_vol_abs',
-                                     'pct_of_nav']]
+    ticker_overall['pct_of_3M_vol'] = ticker_overall['Shares_held_change'] / ticker_overall['3MAvgVol']
+    ticker_overall['pct_of_3M_vol_abs'] = ticker_overall['Shares_held_change_abs'] / ticker_overall['3MAvgVol']
+    ticker_overall = ticker_overall[['hdate', 'fund', 'ticker', 'Price', 'Price_change', 'Price_change_pct', 'mv',
+                                     'Shares_held', 'Shares_held_change', 'Shares_held_change_abs',
+                                     'Shares_held_change_pct', 'Volume', '3MAvgVol', 'pct_of_3M_vol',
+                                     'pct_of_3M_vol_abs', 'pct_of_nav']]
     return ticker_overall
